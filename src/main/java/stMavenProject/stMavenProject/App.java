22b4bcb05a1fc90b77919aa0c70a272f277bc26f
ashwini <jangamashwini04@gmail.com>
2021-02-10 18:50:24 +0530

    Update Pom.xml , Add.java file

--- src/main/java/stMavenProject/stMavenProject/App.java
+++ src/main/java/stMavenProject/stMavenProject/App.java
@@ -1,5 +1,12 @@
 package stMavenProject.stMavenProject;
 
+import javax.sql.rowset.WebRowSet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+         driver.get("https://www.facebook.com");
     }
 }
