04b13ba93660995202b6eab7849d67c251a79ea4
ashwini <jangamashwini04@gmail.com>
2021-02-26 01:07:31 +0530

    pom.xmm and AppTest java file updated

--- src/main/java/stMavenProject/stMavenProject/App.java
+++ src/main/java/stMavenProject/stMavenProject/App.java
@@ -13,10 +13,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        WebDriverManager.chromedriver().setup();
+    public void luanchUrl() {
+    	WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
          driver.get("https://www.facebook.com");
-    }
+    
+	}
+        
 }
--- src/test/java/stMavenProject/stMavenProject/AppTest.java
+++ src/test/java/stMavenProject/stMavenProject/AppTest.java
@@ -1,5 +1,10 @@
 package stMavenProject.stMavenProject;
 
 public class AppTest {
-    
+    public void TC_01() {
+    	App app =new App();
+    	app.luanchUrl();
+    	
+	}
+	
 }
